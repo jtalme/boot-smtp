1c93f04e5aba1e6bab442202bef540c4058e22b5
Jon Alme <jonathanalme@workfront.com>
2016-07-27 22:20:39 -0500

    remove comments from index.js that don't apply anymore

--- src/main/java/com/tallstak/bootsmtp/server/MailController.java
+++ src/main/java/com/tallstak/bootsmtp/server/MailController.java
@@ -2,6 +2,7 @@ package com.tallstak.bootsmtp.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,6 +39,19 @@ public class MailController {
 	@ResponseBody
 	public List<Email> getMail() {
 		return (List<Email>) emailRepository.findAll();
+	}
+
+	/**
+	 * Returns all in-memory emails
+	 * @return
+	 */
+	@RequestMapping(
+		path = "/mail/{id}",
+		method = RequestMethod.GET
+	)
+	@ResponseBody
+	public Email getMail(@PathVariable Long id) {
+		return emailRepository.findOne(id);
 	}
 
 	/**
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,5 @@
-// Define the `phonecatApp` module
 var bootSmtp = angular.module('bootSmtp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
 bootSmtp.controller('EmailController', function EmailController($scope, $http) {
 	$http({
 		method: 'GET',
@@ -9,7 +7,6 @@ bootSmtp.controller('EmailController', function EmailController($scope, $http) {
 	}).then(function successCallback(response) {
 		$scope.emails = response.data;
 	}, function errorCallback(response) {
-		// called asynchronously if an error occurs
-		// or server returns response with an error status.
+		
 	});
 });
